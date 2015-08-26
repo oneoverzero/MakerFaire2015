41c7752f8ae2d9a50baafbf736e5a2c4db9928fd
Luis Correia <info@loide.net>
2015-08-26 08:46:02 +0100

    command list is now a simple char string, no need to convert to int

--- arduino/Arduino2/Arduino2.ino
+++ arduino/Arduino2/Arduino2.ino
@@ -108,8 +108,8 @@ const int turretP = 6;   // the digital pin used for the servo
 
 // command list
 #define listSize 15
-int cmdList[listSize]; // the list
-int currentCommand     = 0;   // active command being executed (check also is we're processingCommands)
+char cmdList[listSize]; // the list
+char currentCommand    = "";   // active command being executed (check also is we're processingCommands)
 int numCommand         = 0;   // command count received
 int cmdPointer         = 0;   // helper var for current command in list
 int processingCommands = 0;   // flag for helping us know if we're running or stopped
@@ -150,7 +150,7 @@ unsigned long commandMillies = 0;   // move rover for these millies
 // clear command list
 void resetCmdList() {
   for (int x = 0; x < listSize; x++) {
-    cmdList[x] = 0;
+    cmdList[x] = "0";
   }
 }
 
@@ -176,7 +176,7 @@ void readSerialLine() {
 
         // copy all commands from within the string
         for ( x = 3; x < inData.length() - 3; x++) {
-          cmdList[x - 3] = inData.charAt(x) - '0';
+          cmdList[x - 3] = inData.charAt(x);
           numCommand++;
         }
         cmdPointer = -1;
@@ -189,9 +189,9 @@ void readSerialLine() {
       // COL000LOC
       if (inData.startsWith("COL") && inData.endsWith("LOC")) {
         // collision warning
-        int fwdleft  = inData.charAt(3) - '0';
-        int fwdright = inData.charAt(4) - '0';
-        int backward = inData.charAt(5) - '0';
+        int fwdleft  = inData.charAt(3);
+        int fwdright = inData.charAt(4);
+        int backward = inData.charAt(5);
 
         // invalidate current command by killing millies
         movingMillies = millis();
@@ -317,21 +317,21 @@ void loop() {
   if (millis() >= nextOutput) {
 
     switch (currentCommand) {
-      case 0:
+      case "0":
         DEBUG_PRINTLN("NOP");
         // TODO turn off LED
         setLEDCommandColor(currentCommand);
 
         break;
 
-      case 1:
+      case "1":
         DEBUG_PRINTLN("STP");
         // rover is NOT moving, stop both motors
         isRoverMoving = 0;
         motorController.stopMoving();
         break;
 
-      case 2:
+      case "2":
         DEBUG_PRINTLN("FWD");
         // only run this bit if not moving
         if (isRoverMoving == 0) {
@@ -349,7 +349,7 @@ void loop() {
         }
         break;
 
-      case 3:
+      case "3":
         DEBUG_PRINTLN("BCK");
         // only run this bit is not moving
         if (isRoverMoving == 0) {
@@ -366,7 +366,7 @@ void loop() {
         }
         break;
 
-      case 4:
+      case "4":
         DEBUG_PRINTLN("ROL");
         // only run this bit is not moving
         if (isRoverMoving == 0) {
@@ -383,7 +383,7 @@ void loop() {
         }
         break;
 
-      case 5:
+      case "5":
         DEBUG_PRINTLN("ROR");
         // only run this bit is not moving
         if (isRoverMoving == 0) {
