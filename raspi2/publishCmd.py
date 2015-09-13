35ebe8eee1b05be670d6d6ec73ffb2045365d1d6
lc oozlabs <lc@labs.oneoverzero.org>
2015-09-13 18:58:46 +0100

    remove unnecessary stuff

--- raspi2/publishCmd.py
+++
@@ -1,5 +0,0 @@
-import paho.mqtt.publish as paho
-from vars import *
-
-paho.single("rover/command","MIS22334455SIM",hostname=mqttServer)
-
--- raspi2/t.py
+++
@@ -1,5 +0,0 @@
-import paho.mqtt.publish as paho
-from vars import *
-
-paho.single("rover/command","MIS2SIM",hostname=mqttServer)
-
