c4e7ac2b3205322a105bb2d6ef54ce146bbe1f5d
Luis Correia <info@loide.net>
2015-10-20 17:03:35 +0100

    add comment

--- mission-control/control/wsmqtt.py
+++ mission-control/control/wsmqtt.py
@@ -6,6 +6,7 @@ class WS(object):
   @cherrypy.expose
   def index(self,msg=""):
     print "got this! " + msg
+    # if message is correct, publish command into MQTT
     if msg[:3] == 'MIS' and msg[-3:] == 'SIM':
       paho.single("rover/command",msg,hostname=mqttServer)
     return "ok"
